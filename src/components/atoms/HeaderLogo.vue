012ccb0109209c0e9633ebf1bf8ba3d896c7a89b
hayashida-bulanco <hayashida@bulanco.net>
2019-07-14 02:03:48 +0900

    [Remove] Remove some dead code from HeaderLogo

--- src/components/atoms/HeaderLogo.vue
+++ src/components/atoms/HeaderLogo.vue
@@ -7,15 +7,12 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class HelloWorld extends Vue {
-  // @Prop() private msg!: string;
-}
+export default class HeaderLogo extends Vue {}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .header-logo {
   width: 60px;
