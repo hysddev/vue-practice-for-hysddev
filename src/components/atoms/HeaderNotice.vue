884c1d130cd4c452ecf50b0b14276a7f57717d3c
hayashida-bulanco <hayashida@bulanco.net>
2019-07-13 23:22:34 +0900

    [Add] Add compornent HeaderNotice

---
+++ src/components/atoms/HeaderNotice.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header-notice">{{ number }}</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HeaderNotice extends Vue {
+  @Prop() private number!: number;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header-notice {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 32px;
+  height: 32px;
+  background-color: #edfce2;
+  color: #3f9200;
+  border-radius: 4px;
+  font-size: 1.4rem;
+  position: relative;
+  cursor: pointer;
+  user-select: none;
+  margin-right: 16px;
+  outline: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
     <header-logo/>
+    <header-notice number="0"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import HeaderLogo from '@/components/atoms/HeaderLogo.vue'
+import HeaderNotice from '@/components/atoms/HeaderNotice.vue'
 
 @Component({
   components: {
-    HeaderLogo
+    HeaderLogo,
+    HeaderNotice
   }
 })
+
 export default class Home extends Vue {}
 </script>
